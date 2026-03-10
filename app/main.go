7993012943830a5e4e7866ceea49fe0541ab80df
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-03-10 22:37:30 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,22 +8,25 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	for {
 		fmt.Print("$ ")
+
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 		command = strings.TrimSpace(command)
+
 		if command == "exit" {
 			break
+		} else if strings.HasPrefix(command, "echo ") {
+			command = strings.TrimPrefix(command, "echo ")
+			fmt.Println(command)
+		} else {
+			fmt.Printf("%s: command not found\n", command)
 		}
-		fmt.Printf("%s: command not found\n", command)
 	}
 
 }
