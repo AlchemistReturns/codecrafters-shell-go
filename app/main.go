9ae755dce087cdc6d33d56090cef5f8ea3778ea0
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-03-10 23:42:17 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,25 +11,32 @@ import (
 )
 
 func main() {
+	//REPL
 	for {
 		fmt.Print("$ ")
 
+		//Read the command and trim
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 		command = strings.TrimSpace(command)
 
+		//array to maintain builtin types
 		builtinTypes := []string{"type", "echo", "exit"}
 
 		if command == "exit" {
 			break
 
 		} else if strings.HasPrefix(command, "echo ") {
+			//echoes a command
 			command = strings.TrimPrefix(command, "echo ")
 			fmt.Println(command)
 
 		} else if strings.HasPrefix(command, "type ") {
+			//Checks the command type
+			//If is a builtin, displays that it is a builtin command
+			//Else shows the absolute file path
 			command = strings.TrimPrefix(command, "type ")
 
 			if slices.Contains(builtinTypes, command) {
@@ -44,6 +51,10 @@ func main() {
 			}
 
 		} else {
+			//splits the command line arguments into an argv slice
+			//checks the first argument to see if it is an executable
+			//If it is an executable, calls the execute command with the provided arguments
+			//Displays the output of executing the executable
 			argv := strings.Split(command, " ")
 			_, err := exec.LookPath(argv[0])
 
