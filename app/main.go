1c66af0d2adc51c8de1a245ae3120a3d7d9c7e61
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-03-10 23:23:18 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -24,11 +24,14 @@ func main() {
 
 		if command == "exit" {
 			break
+
 		} else if strings.HasPrefix(command, "echo ") {
 			command = strings.TrimPrefix(command, "echo ")
 			fmt.Println(command)
+
 		} else if strings.HasPrefix(command, "type ") {
 			command = strings.TrimPrefix(command, "type ")
+
 			if slices.Contains(builtinTypes, command) {
 				fmt.Printf("%s is a shell builtin\n", command)
 			} else {
@@ -39,8 +42,22 @@ func main() {
 					fmt.Printf("%s is %s\n", command, path)
 				}
 			}
+
 		} else {
-			fmt.Printf("%s: command not found\n", command)
+			argv := strings.Split(command, " ")
+			_, err := exec.LookPath(argv[0])
+
+			if err != nil {
+				fmt.Printf("%s: command not found\n", command)
+			} else {
+				cmd := exec.Command(argv[0], argv[1:]...)
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Printf("%s: %s\n", command, err)
+				} else {
+					fmt.Printf("%s\n", output)
+				}
+			}
 
 		}
 	}
