3358ac7003a0b81708f44145b1888f6ad8dce3cf
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-03-12 02:53:01 +0600

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,9 +6,34 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"slices"
 	"strings"
 )
+
+func handleInput(input string) []string {
+	// This regex matches either:
+	// 1. A quoted string: '((?:''|[^'])*)'
+	// 2. A sequence of non-space, non-quote characters: ([^'\s]+)
+	re := regexp.MustCompile(`'((?:''|[^'])*)'|([^'\s]+)`)
+
+	matches := re.FindAllStringSubmatch(input, -1)
+	var result []string
+
+	for _, m := range matches {
+		if m[1] != "" || strings.Contains(m[0], "''") || m[0] == "''" {
+			// It's a quoted match (Group 1)
+			// Dissolve '' into a single ' (or empty string "" per your preference)
+			dissolved := strings.ReplaceAll(m[1], "''", "")
+			result = append(result, dissolved)
+		} else if m[2] != "" {
+			// It's a plain word (Group 2)
+			result = append(result, m[2])
+		}
+	}
+
+	return result
+}
 
 func main() {
 	//array to maintain builtin types
@@ -19,36 +44,39 @@ func main() {
 		fmt.Print("$ ")
 
 		//Read the command and trim
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		command = strings.TrimSpace(command)
+		input = strings.TrimSpace(input)
 
 		//Split the command
-		argv := strings.Split(command, " ")
+		command := strings.Split(input, " ")[0]
+		args := strings.Join(strings.Split(input, " ")[1:], " ")
+		argv := handleInput(args)
 
 		//Perform according to the input command
-		switch argv[0] {
+		switch command {
 		case "exit":
 			os.Exit(0)
 
 		case "echo":
-			fmt.Println(strings.Join(argv[1:], " "))
+			output := strings.Join(argv, " ")
+			fmt.Println(output)
 
 		case "type":
 			//Checks the command type
 			//If is a builtin, displays that it is a builtin command
 			//Else shows the absolute file path
 
-			if slices.Contains(builtinTypes, argv[1]) {
-				fmt.Printf("%s is a shell builtin\n", argv[1])
+			if slices.Contains(builtinTypes, argv[0]) {
+				fmt.Printf("%s is a shell builtin\n", argv[0])
 			} else {
-				path, err := exec.LookPath(argv[1])
+				path, err := exec.LookPath(argv[0])
 				if err != nil {
-					fmt.Printf("%s: not found\n", argv[1])
+					fmt.Printf("%s: not found\n", argv[0])
 				} else {
-					fmt.Printf("%s is %s\n", argv[1], path)
+					fmt.Printf("%s is %s\n", argv[0], path)
 				}
 			}
 
@@ -64,13 +92,13 @@ func main() {
 		case "cd":
 			//Changes the current working directory
 
-			if argv[1] == "~" {
-				argv[1] = os.Getenv("HOME")
+			if argv[0] == "~" {
+				argv[0] = os.Getenv("HOME")
 			}
 
-			err := os.Chdir(argv[1])
+			err := os.Chdir(argv[0])
 			if err != nil {
-				fmt.Printf("cd: %s: No such file or directory\n", argv[1])
+				fmt.Printf("cd: %s: No such file or directory\n", argv[0])
 			}
 
 		default:
@@ -78,15 +106,15 @@ func main() {
 			//If it is an executable, calls the execute command with the provided arguments
 			//Displays the output of executing the executable
 
-			_, err := exec.LookPath(argv[0])
+			_, err := exec.LookPath(command)
 
 			if err != nil {
-				fmt.Printf("%s: command not found\n", argv[0])
+				fmt.Printf("%s: command not found\n", command)
 			} else {
-				cmd := exec.Command(argv[0], argv[1:]...)
+				cmd := exec.Command(command, argv[0:]...)
 				output, err := cmd.CombinedOutput()
 				if err != nil {
-					fmt.Printf("%s: %s\n", argv[0], err)
+					fmt.Printf("%s: %s\n", command, err)
 				} else {
 					fmt.Printf("%s", output)
 				}
